e6f963ad7fa1cff13deed928960290d090ab3d0f
Rubenslourenco <alvesrubens926@gmail.com>
2025-05-06 17:13:26 -0300

    feat: feito alterção no models criando coneção com o banco de dados

--- Enums/FuncionarioModel.cs
+++ Enums/FuncionarioModel.cs
@@ -1,18 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Enums;
 
 namespace WebAPI.Models
 {
-    public class FuncionarioModels
+    public class FuncionarioModel
     {
+        [Key]
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
-        public string Departamento { get; set; }
+        public DepartmentEnum Departamento { get; set; }
         public bool Ativo { get; set; }
-        public string Turno { get; set; }
+        public TurnoEnum Turno { get; set; }
         public DateTime DataDeCriação { get; set; } = DateTime.Now.ToLocalTime();
         public DateTime DataDeAlteracao { get; set; } = DateTime.Now.ToLocalTime();
 
